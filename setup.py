a864a254078a09cf158e1d0b5f1083f0410a22ac
Neil Wallace <rowinggolfer@googlemail.com>
2009-08-16 11:51:09 +0100

    setup.py works. unable to build a working deb as yet though.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import glob
+
+setup(
+    name = 'openmolar',
+    version = '0.1.2',
+    description = 'Open Source Dental Practice Management Software',
+    author = 'Neil Wallace',
+    author_email = 'rowinggolfer@googlemail.com',
+    url = 'https://launchpad.net/openmolar',
+    license = 'GPL v3',
+    scripts=['openmolar'],
+    )
+
