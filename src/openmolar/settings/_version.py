60c0111333b0b07760754027abd6ac132598b11d
Neil Wallace <rowinggolfer@googlemail.com>
2009-10-26 17:01:42 +0000

    new setup.py again.

--- src/openmolar/settings/_version.py
+++ src/openmolar/settings/_version.py
@@ -5,11 +5,11 @@ So don't edit it. :)
 """
 
 version_info = {'branch_nick': u'openmolar',
- 'build_date': '2009-10-26 16:57:00 +0000',
+ 'build_date': '2009-10-26 17:01:42 +0000',
  'clean': None,
- 'date': '2009-10-26 16:27:10 +0000',
- 'revision_id': 'rowinggolfer@googlemail.com-20091026162710-17o1vyzbek3giw57',
- 'revno': 400}
+ 'date': '2009-10-26 16:57:00 +0000',
+ 'revision_id': 'rowinggolfer@googlemail.com-20091026165700-1x027dqjh3vhx7te',
+ 'revno': 401}
 
 revisions = {}
 
