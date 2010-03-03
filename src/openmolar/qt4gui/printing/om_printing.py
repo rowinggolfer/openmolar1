e245f8a666f806db0cb5071883c873e9cfc6e52e
Neil Wallace <rowinggolfer@googlemail.com>
2010-03-03 16:40:16 +0000

    1 line bugfix

--- src/openmolar/qt4gui/printing/om_printing.py
+++ src/openmolar/qt4gui/printing/om_printing.py
@@ -262,7 +262,7 @@ def printReferral(om_gui):
     #    return
     html=referral.getHtml(desc, om_gui.pt)
     Dialog = QtGui.QDialog(om_gui,  QtCore.Qt.WindowMinimizeButtonHint)
-    t.WindowMinimizeButtonHint
+    
     dl = Ui_enter_letter_text.Ui_Dialog()
     dl.setupUi(Dialog)
     dl.textEdit.setHtml(html)
