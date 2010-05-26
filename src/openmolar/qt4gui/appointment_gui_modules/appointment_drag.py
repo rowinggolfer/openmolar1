bb25bf8d69a5edebcee0036fdc562782c9399442
Neil Wallace <rowinggolfer@googlemail.com>
2010-05-26 13:55:13 +0100

    fixed the mouse down issue by re-implementing mouse Release

--- src/openmolar/qt4gui/appointment_gui_modules/appointment_drag.py
+++ src/openmolar/qt4gui/appointment_gui_modules/appointment_drag.py
@@ -4,9 +4,9 @@ import pickle
 import sys
 
 from PyQt4 import QtGui, QtCore
-from openmolar.dbtools import appointments 
+from openmolar.dbtools import appointments
 from openmolar.settings import localsettings
-        
+
 
 class clinicianSelectModel(QtCore.QAbstractListModel):
     def __init__(self, om_gui):
@@ -15,13 +15,13 @@ class clinicianSelectModel(QtCore.QAbstractListModel):
         self.options_list = [_("Selected Book(s)"), _("Relevant Books"),
         _("Available Dentists"), _("Available Hygenists"),
         _("Available Clinicians"), _("Everyone"), _("Manual Selection")]
-        
+
         self.om_gui = om_gui
         #if localsettings.activehygs == []:
         #    self.options_list.remove(_("Available Hygenists"))
-        
+
         self.manual_index = 6 #used if manual is called by another widget
-        
+
     def rowCount(self, parent = QtCore.QModelIndex()):
         return len(self.options_list)
 
@@ -32,32 +32,32 @@ class clinicianSelectModel(QtCore.QAbstractListModel):
             option = self.options_list[index.row()]
             return QtCore.QVariant(option)
         return QtCore.QVariant()
-    
+
     def clinician_list(self, row, date):
         if row == 0:
             chkset = self.om_gui.apt_drag_model.selectedClinicians
-            return appointments.getWorkingDents(date, chkset, 
+            return appointments.getWorkingDents(date, chkset,
                 include_non_working = True)
         if row == 1:
             chkset = self.om_gui.apt_drag_model.involvedClinicians
-            return appointments.getWorkingDents(date, chkset, 
+            return appointments.getWorkingDents(date, chkset,
                 include_non_working = True)
         elif row == 2:
             chkset = localsettings.activedent_ixs
-            return appointments.getWorkingDents(date, chkset, 
-                include_non_working=False)            
+            return appointments.getWorkingDents(date, chkset,
+                include_non_working=False)
         elif row == 3:
             chkset = localsettings.activehyg_ixs
-            return appointments.getWorkingDents(date, chkset, 
-                include_non_working=False)            
+            return appointments.getWorkingDents(date, chkset,
+                include_non_working=False)
         elif row == 4:
-            return appointments.getWorkingDents(date, 
-                include_non_working=False)                        
+            return appointments.getWorkingDents(date,
+                include_non_working=False)
         elif row == 5:
             return appointments.getWorkingDents(date)
         elif row == 6:
             return False
-    
+
 class simple_model(QtCore.QAbstractListModel):
     def __init__(self, parent=None):
         super(simple_model, self).__init__(parent)
@@ -74,7 +74,7 @@ class simple_model(QtCore.QAbstractListModel):
         self.selected_icon = QtGui.QIcon()
         self.selected_icon.addPixmap(
             QtGui.QPixmap(":/icons/schedule_active.png"))
-        
+
     def clear(self):
         self.unscheduledList = []
         self.scheduledList = []
@@ -85,14 +85,14 @@ class simple_model(QtCore.QAbstractListModel):
     @property
     def involvedClinicians(self):
         '''
-        returns a set containing all clinicians referred to by the lists 
+        returns a set containing all clinicians referred to by the lists
         within
         '''
         retarg = set()
         for app in self.list:
             retarg.add(app.dent)
         return tuple(retarg)
-    
+
     @property
     def selectedClinicians(self):
         '''
@@ -112,21 +112,21 @@ class simple_model(QtCore.QAbstractListModel):
         self.unscheduledList = []
         self.scheduledList = []
         self.list = []
-            
+
         currentClinicians = self.involvedClinicians
         changedClinicians = True
-        
+
         for appt in appts:
             if appt.past:
                 pass
             elif appt.unscheduled:
                 self.unscheduledList.append(appt)
             else:
-                self.scheduledList.append(appt)            
-            
+                self.scheduledList.append(appt)
+
             if not appt.past and not (appt.dent in currentClinicians):
                 changedClinicians = True
-                
+
         self.list = self.scheduledList + self.unscheduledList
         self.reset()
 
@@ -136,8 +136,8 @@ class simple_model(QtCore.QAbstractListModel):
         if selectedAppt in appts:
             self.setSelectedAppt(selectedAppt)
         else:
-            self.setSelectedAppt(None)        
-        
+            self.setSelectedAppt(None)
+
     def rowCount(self, parent = QtCore.QModelIndex()):
         return len(self.list)
 
@@ -149,10 +149,10 @@ class simple_model(QtCore.QAbstractListModel):
             if app.flag == -128:
                 info = "%s (%s %s)"% (app.name, app.length, _("mins"))
             elif app.unscheduled:
-                info = "%s %s %s %s %s"% (app.length, _("mins"), 
+                info = "%s %s %s %s %s"% (app.length, _("mins"),
                     app.trt1, _("with"), app.dent_inits)
             else:
-                info = "%s %s with %s"% (app.readableDate, 
+                info = "%s %s with %s"% (app.readableDate,
                     app.readableTime, app.dent_inits)
             return QtCore.QVariant(info)
         elif role == QtCore.Qt.ForegroundRole:
@@ -183,11 +183,11 @@ class simple_model(QtCore.QAbstractListModel):
         try:
             index = self.index(self.list.index(appt))
             self.min_slot_length = appt.length
-            self.selection_model.select(index, 
+            self.selection_model.select(index,
                 QtGui.QItemSelectionModel.SelectCurrent)
         except ValueError:
             self.selection_model.clear()
-            
+
 
 class blockModel(simple_model):
     '''
@@ -197,12 +197,12 @@ class blockModel(simple_model):
         super(blockModel, self).__init__(parent)
         self.list = []
         for val, length in (
-        (_("Lunch"), 60), 
+        (_("Lunch"), 60),
         (_("Lunch"), 30),
         (_("staff meeting"), 10),
         (_("emergency"), 15),
         (_("emergency"), 20),
-        (_("emergency"), 30),        
+        (_("emergency"), 30),
         (_("Out of Office"), 30)):
             block = appointments.appt_class()
             block.name = val
@@ -210,7 +210,7 @@ class blockModel(simple_model):
             block.length = length
             block.flag = -128
             self.list.append(block)
-            
+
     def reset(self):
         pass
 
@@ -223,9 +223,9 @@ class draggableList(QtGui.QListView):
         self.setDragEnabled(True)
         if multiSelect:
             self.setSelectionMode(QtGui.QAbstractItemView.MultiSelection)
-        self.pixels_per_min = 2    
+        self.pixels_per_min = 2
         self.setMinimumHeight(150)
-        
+
     def setScaling(self, height_per_minute):
         '''
         make the list aware of the scaling of the widget available for drops
@@ -245,8 +245,9 @@ class draggableList(QtGui.QListView):
         if not selectedApp.unscheduled:
             event.ignore()
             return
-        self.setCurrentIndex(index)
-        
+        if not self.currentIndex() == index:
+            self.setCurrentIndex(index)
+
         ## convert to  a bytestream
         bstream = cPickle.dumps(selectedApp)
         mimeData = QtCore.QMimeData()
@@ -259,34 +260,32 @@ class draggableList(QtGui.QListView):
         #pmap = QtGui.QPixmap(50 , selectedApp.length * self.pixels_per_min)
         #pmap.fill(QtGui.QColor(127,0,0))
         #drag.setHotSpot(QtCore.QPoint(pmap.width()/2, pmap.height()/2)
-                
+
         pixmap = QtGui.QPixmap()
         pixmap = pixmap.grabWidget(self, self.rectForIndex(index))
         drag.setPixmap(pixmap)
-        drag.setHotSpot(QtCore.QPoint(-10,0)) 
-        
-        drag.start(QtCore.Qt.CopyAction)
-    
-    def mouseMoveEvent(self, event):
-        self.startDrag(event)
-    
+        drag.setHotSpot(QtCore.QPoint(-10,0))
 
-    ##TODO
-    #
-    #def mousePressEvent(self, event):
-    #    print "mousePress"
-    #    event.ignore()
-    #    
-    #def mouseReleaseEvent(self, event):
-    #    print "mouseRelease"
-    #    event = QtGui.QMouseEvent(event)
-    #    QtGui.QListView.mousePressEvent(self, event)
-        
+        drag.start(QtCore.Qt.CopyAction)
+
+    def mouseMoveEvent(self, event):
+        print "mouse move"
+        self.startDrag(event)
+
+    def mousePressEvent(self, event):
+        ##set this to ignore, and use mouse Release instead
+        event.ignore()
+
+    def mouseReleaseEvent(self, event):
+        event = QtGui.QMouseEvent(QtCore.QEvent.MouseButtonPress, event.pos(),
+            event.button(), event.buttons(), QtCore.Qt.NoModifier)
+        QtGui.QListView.mousePressEvent(self, event)
+
     def selectionChanged (self, selectedRange, deselected):
         '''
         the user has selected an appointment (or range of appointments!)
         from the list
-        
+
         currently, the model is a single selection
         '''
         if selectedRange.count():
@@ -295,9 +294,9 @@ class draggableList(QtGui.QListView):
             selected = None
         rows = self.selectionModel().selectedRows()
         self.model().setSelectedIndexes(rows, selected)
-        
+
         self.doItemsLayout()
-        
+
 if __name__ == "__main__":
     '''
     the try catch here is to ensure that the app exits cleanly no matter what
@@ -309,10 +308,10 @@ if __name__ == "__main__":
     from openmolar.dbtools import appointments
     from openmolar.settings import localsettings
     localsettings.initiate()
-    
+
     def slot_catcher(arg):
         print arg
-    
+
     class duckPt(object):
         def __init__(self):
             self.serialno = 707
@@ -326,53 +325,53 @@ if __name__ == "__main__":
             super(testDialog, self).__init__(parent)
             self.setWindowTitle("Drag Drop Test")
             self.pt = duckPt()
-            
+
             layout = QtGui.QGridLayout(self)
 
             self.model = simple_model()
-    
+
             appts = appointments.get_pts_appts(duckPt())
-    
+
             self.model.setAppointments(appts, appts[2])
 
             self.daylistView = draggableList(True)
             self.daylistView.setModel(self.model)
             self.daylistView.setSelectionModel(self.model.selection_model)
-            
+
             self.weeklistView = draggableList(True)
             self.weeklistView.setModel(self.model)
             self.weeklistView.setSelectionModel(
                 self.daylistView.selectionModel())
-    
+
             self.block_model = blockModel()
             self.blockView = draggableList(False)
             self.blockView.setModel(self.block_model)
-            
+
             self.book = appointmentwidget.appointmentWidget(self, "1000",
             "1200")
             self.book.setDentist(1)
             self.book.setStartTime(1015)
             self.book.setEndTime(1145)
             for appoint in (
-            (1, 1030, 1045, 'MCDONALD I', 6155L, 'EXAM', '', '', '', 1, 73, 0, 
+            (1, 1030, 1045, 'MCDONALD I', 6155L, 'EXAM', '', '', '', 1, 73, 0,
             0, datetime.datetime.now())
 
-            ,(1, 1115, 1130, 'EMERGENCY', 0L, '', '', '', '', -128, 0, 0, 
+            ,(1, 1115, 1130, 'EMERGENCY', 0L, '', '', '', '', -128, 0, 0,
             0, datetime.datetime.now())):
                 self.book.setAppointment(appoint)
 
             self.OVbook = appointment_overviewwidget.bookWidget(1,
             "1000", "1200", 15, 2, self)
-            
+
             d1 = appointments.dentistDay(1)
             d1.start=1015
             d1.end=1145
             d1.memo="hello"
-            
+
             d2 = appointments.dentistDay(4)
             d2.start=1015
             d2.end=1145
-            
+
             self.OVbook.dents=[d1,d2]
             self.OVbook.clear()
             self.OVbook.init_dicts()
@@ -387,10 +386,10 @@ if __name__ == "__main__":
                 1,20)
             slot2 = appointments.freeSlot(datetime.datetime(2009,2,2,11,05),
                 4,60)
-            
+
             self.OVbook.addSlot(slot)
             self.OVbook.addSlot(slot2)
-            
+
             appt = appointments.aowAppt()
             appt.mpm = 10*60+30
             appt.length = 15
@@ -411,7 +410,7 @@ if __name__ == "__main__":
             layout.addWidget(self.daylistView,0,0)
             layout.addWidget(self.weeklistView,1,0)
             layout.addWidget(self.blockView,2,0)
-            
+
             layout.addWidget(self.tw,0,1,3,1)
             #self.tw.setCurrentIndex(1)
 
@@ -422,7 +421,7 @@ if __name__ == "__main__":
                     self.weeklistView.setScaling)
             self.connect(self.book, QtCore.SIGNAL("redrawn"),
                     self.daylistView.setScaling)
-            
+
     try:
         app = QtGui.QApplication([])
         dl = testDialog()
