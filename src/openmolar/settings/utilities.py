78f2eefc512139f4e9a368c8ce46abd01176c245
Neil Wallace <rowinggolfer@googlemail.com>
2013-09-03 21:58:52 +0100

    simple changes to one file

--- src/openmolar/settings/utilities.py
+++ src/openmolar/settings/utilities.py
@@ -7,31 +7,36 @@
 # for more details.
 
 import os
+import logging
+
 from openmolar.settings import localsettings
+
+LOGGER = logging.getLogger("openmolar")
 
 def getPDF():
     '''
     get's the pdf which has been created to local file during some print proc
     '''
     try:
-        f = open(localsettings.TEMP_PDF,"rb")
+        f = open(localsettings.TEMP_PDF, "rb")
         data = f.read()
         f.close()
         return data
-    except Exception,e:
-        print "error in utilities.getPdf"
-        print Exception,e
+    except Exception as exc:
+        LOGGER.exception("exception in utilities.getPdf")
         
 def deleteTempFiles():
     '''
     delete's any temprorary pdf file
     '''
-    print "deleting temporary Files"
+    LOGGER.info("deleting temporary Files")
     for name in ("import_temp", "temp.pdf"):
         fpath = os.path.join(localsettings.localFileDirectory, name)
         if os.path.exists(fpath):
             os.remove(fpath)
     
 if __name__ == "__main__":
-    '''testing only'''
-    
+    '''
+    testing only
+    '''
+    pass
