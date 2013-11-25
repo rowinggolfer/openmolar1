6acfe59c3e09e0dc31eea2636fd6deea91707aa7
Neil Wallace <rowinggolfer@gmail.com>
2013-11-25 16:08:14 +0000

    ready to build v0.5.0-alpha5

--- src/openmolar/settings/version.py
+++ src/openmolar/settings/version.py
@@ -10,7 +10,7 @@ LOGGER = logging.getLogger("openmolar")
 #this is a fallback version, which will be used if
 #openmolar is not run from a git repository
 #or ig git is not installed.
-VERSION = "0.5.0-alpha4"
+VERSION = "0.5.0-alpha5"
 
 try:
     p = subprocess.Popen(
