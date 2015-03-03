cebf60f4e7c927c617f2feff454a43571b9fd159
rowinggolfer <rowinggolfer@gmail.com>
2015-03-03 14:38:41 +0000

    ropeproject added to git ignore, and pep8 for __init__.py

--- src/openmolar/__init__.py
+++ src/openmolar/__init__.py
@@ -1,26 +1,31 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# ############################################################################ #
-# #                                                                          # #
-# # Copyright (c) 2009-2014 Neil Wallace <neil@openmolar.com>                # #
-# #                                                                          # #
-# # This file is part of OpenMolar.                                          # #
-# #                                                                          # #
-# # OpenMolar is free software: you can redistribute it and/or modify        # #
-# # it under the terms of the GNU General Public License as published by     # #
-# # the Free Software Foundation, either version 3 of the License, or        # #
-# # (at your option) any later version.                                      # #
-# #                                                                          # #
-# # OpenMolar is distributed in the hope that it will be useful,             # #
-# # but WITHOUT ANY WARRANTY; without even the implied warranty of           # #
-# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            # #
-# # GNU General Public License for more details.                             # #
-# #                                                                          # #
-# # You should have received a copy of the GNU General Public License        # #
-# # along with OpenMolar.  If not, see <http://www.gnu.org/licenses/>.       # #
-# #                                                                          # #
-# ############################################################################ #
+# ########################################################################### #
+# #                                                                         # #
+# # Copyright (c) 2009-2014 Neil Wallace <neil@openmolar.com>               # #
+# #                                                                         # #
+# # This file is part of OpenMolar.                                         # #
+# #                                                                         # #
+# # OpenMolar is free software: you can redistribute it and/or modify       # #
+# # it under the terms of the GNU General Public License as published by    # #
+# # the Free Software Foundation, either version 3 of the License, or       # #
+# # (at your option) any later version.                                     # #
+# #                                                                         # #
+# # OpenMolar is distributed in the hope that it will be useful,            # #
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of          # #
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           # #
+# # GNU General Public License for more details.                            # #
+# #                                                                         # #
+# # You should have received a copy of the GNU General Public License       # #
+# # along with OpenMolar.  If not, see <http://www.gnu.org/licenses/>.      # #
+# #                                                                         # #
+# ########################################################################### #
+
+'''
+Whenever openmolar is imported, this module ensures the environment is
+sane by initialising gettext and logging.
+'''
 
 import os
 import sys
@@ -49,18 +54,18 @@ elif "-v" in sys.argv:
 else:
     LOGGER.setLevel(logging.INFO)
 
-LOGGER.debug("running openmolar base module = %s" % os.path.dirname(__file__))
+LOGGER.debug("running openmolar base module = %s", os.path.dirname(__file__))
 
 lang = os.environ.get("LANG")
 if lang:
     try:
-        LOGGER.debug("trying to install your environment language %s" % lang)
+        LOGGER.debug("trying to install your environment language %s", lang)
         lang1 = gettext.translation('openmolar', languages=[lang, ])
         lang1.install(unicode=True)
     except IOError:
-        LOGGER.warning("%s not found, using default" % lang)
+        LOGGER.warning("%s not found, using default", lang)
         gettext.install('openmolar', unicode=True)
 else:
-    #-- on windows.. os.environ.get("LANG") is None
+    # - on windows.. os.environ.get("LANG") is None
     LOGGER.warning("no language environment found (windows?)")
     gettext.install('openmolar', unicode=True)
